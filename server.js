e608f7c54db194352ed6f75dcd833a94b582a5d1
Jeffrey davies <jeffpdavies@gmail.com>
2020-03-17 17:55:35 +1100

    added files

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-app.get("/", (req, res) => res.send("API running"));
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
 
